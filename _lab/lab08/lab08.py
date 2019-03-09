143cf60b4ac799235b9fbf6852b71737a2f7c77a
Mohith Pukale <mohithrp93@gmail.com>
2019-03-09 00:24:33 -0800

    Adding extra comments about decorator in lab08

--- _lab/lab08/lab08.py
+++ _lab/lab08/lab08.py
@@ -1,6 +1,7 @@
 # Student(s): (insert name here)
 # Make sure to read the comments for each function.
 
+# Do not delete this method, this is to help grade your subsmissions
 def call_counter(func):
     def helper(*args, **kwargs):
         helper.calls += 1
@@ -9,7 +10,7 @@ def call_counter(func):
     helper.__name__= func.__name__
     return helper
 
-
+# Make sure @call_counter is present above every method (Do not delete it)
 @call_counter
 def recursiveDigitSum(n):
     '''
